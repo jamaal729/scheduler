5c2e15e2efa74dcff622519d25ca59ee35f1c0b0
Jamaal Ahmed <jamaal729@yahoo.com>
2019-11-26 07:49:19 -0700

    Add appointment component.

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import classnames from "classnames/bind";
+
+import "components/Application.scss";
+
+export default function Appointment(props) {
+  const appointmentClass = classnames("appointment", {
+    //
+  });
+
+  return <article className="appointment"></article>;
+}
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import InterviewerListItem from "components/InterviewerListItem";
 import DayList from "components/DayList";
 import InterviewerList from "components/InterviewerList";
 
+import Appointment from "components/Appointment";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -126,3 +128,10 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
