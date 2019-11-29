14b72d81e4f91d89e3f4f0068ed0af3466568263
Jamaal Ahmed <jamaal729@yahoo.com>
2019-11-28 21:23:04 -0700

    Add interviewers selector.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,21 @@
 export const getAppointmentsForDay = (state, stateday) => {
   const appointments = [];
-
   const day = state.days.filter(d => d.name === stateday);
   if (day.length === 0) {
     return appointments;
   }
   day[0].appointments.map(id => appointments.push(state.appointments[id]));
   return appointments;
+};
+
+export const getInterviewersForDay = (state, stateday) => {
+  const interviewers = [];
+  const day = state.days.filter(d => d.name === stateday);
+  if (day.length === 0) {
+    return interviewers;
+  }
+  day[0].interviewers.map(id => interviewers.push(state.interviewers[id]));
+  return interviewers;
 };
 
 export const getInterview = (state, stateinterview) => {
