afc889581ff0bca6811a19614f19ee4d44faacfb
Jamaal Ahmed <jamaal729@yahoo.com>
2019-11-28 19:08:51 -0700

    Add visual custom hooks.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,10 +1,26 @@
-import react, { useState } from "react";
+// import react, { useState } from "react";
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
+  function transition(newMode, replace = false) {
     setMode(newMode);
+    if (replace) {
+      history.pop();
+    }
+    history.push(newMode);
   }
-  return { mode, transition };
+
+  function back() {
+    if (history.length > 1) {
+      const newHistory = history;
+      newHistory.pop();
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
 }
