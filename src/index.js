010763657fb2035751011fa78b40c3c59b8ea924
Jamaal <jamaal729@yahoo.com>
2019-12-03 00:20:57 -0700

    Remove un-used util file.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "index.scss";
 import Application from "components/Application";
 
 
-import consolePrint from './util';
-let printItem = "hello";
-consolePrint({ printItem });
+// import consolePrint from './util';
+// let printItem = "hello";
+// consolePrint({ printItem });
 
 ReactDOM.render(<Application />, document.getElementById("root"));
